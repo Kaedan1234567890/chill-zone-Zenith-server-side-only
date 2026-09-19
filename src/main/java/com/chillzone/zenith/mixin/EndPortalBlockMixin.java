dd8b42fa353b16a8cd60f4033fc35c7c196e21a1
K_Phoenix16 <Hackerbroke13@gmail.com>
2026-09-19 10:43:25 -0400

    0.11.0-alpha-balance1-endlock-fix2-server-side

--- src/main/java/com/chillzone/zenith/mixin/EndPortalBlockMixin.java
+++ src/main/java/com/chillzone/zenith/mixin/EndPortalBlockMixin.java
@@ -8,7 +8,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EndPortalBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.InsideBlockEffectApplier;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
